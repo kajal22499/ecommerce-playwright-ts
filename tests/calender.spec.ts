76be3c30b0b4dce500400e0270853ff02443e249
kajal22499 <kajalbhosle1999@gmail.com>
2026-07-07 18:37:22 +0530

    calender study

--- tests/calender.spec.ts
+++ tests/calender.spec.ts
@@ -7,8 +7,16 @@ test('calendor selection', async({page})=>
     const year = "2027";
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     await page.locator('.react-date-picker__inputGroup').click();
-    await page.locator('')
-    
+    await page.locator('.react-calendar__navigation__label').click();
+    await page.locator('.react-calendar__navigation__label').click();
+    await page.getByText(year).click();
+    await page.locator('.react-calendar__year-view__months__month').nth(Number(month)-1).click();
+    await page.locator("//abbr[text()='"+date+"']").click();
+
+
+   
+
+
 
 
 });
